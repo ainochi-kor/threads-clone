521db874d9c31306308cf1f2db1f343dba222567
ainochi-kor <kthrkdals@gmail.com>
2025-06-29 00:17:04 +0900

    조건에 따라 내비게이션 다르게 하기

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,95 +1,151 @@
 import { Ionicons } from "@expo/vector-icons";
 import { Tabs, useRouter } from "expo-router";
+import { useState } from "react";
+import { Modal, Text, TouchableOpacity, View } from "react-native";
 
 export default function TabLayout() {
   const router = useRouter();
+  const isLoggedIn = false;
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+
+  const openLoginModal = () => {
+    setIsLoginModalOpen(true);
+    console.log("로그인 모달 열기");
+  };
+
+  const closeLoginModal = () => {
+    setIsLoginModalOpen(false);
+    console.log("로그인 모달 닫기");
+  };
 
   return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-      }}
-    >
-      <Tabs.Screen
-        name="(home)"
-        options={{
-          tabBarLabel: () => null,
-          tabBarIcon: ({ focused }) => (
-            <Ionicons
-              name="home"
-              size={24}
-              color={focused ? "black" : "gray"}
-            />
-          ),
+    <>
+      <Tabs
+        screenOptions={{
+          headerShown: false,
         }}
-      />
-      <Tabs.Screen
-        name="search"
-        options={{
-          tabBarLabel: () => null,
-          tabBarIcon: ({ focused }) => (
-            <Ionicons
-              name="search"
-              size={24}
-              color={focused ? "black" : "gray"}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="add"
-        listeners={{
-          tabPress: (e) => {
-            e.preventDefault();
-            router.navigate("./modal");
-          },
-        }}
-        options={{
-          tabBarLabel: () => null,
-          tabBarIcon: ({ focused }) => (
-            <Ionicons name="add" size={24} color={focused ? "black" : "gray"} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="activity"
-        options={{
-          tabBarLabel: () => null,
-          tabBarIcon: ({ focused }) => (
-            <Ionicons
-              name="heart-outline"
-              size={24}
-              color={focused ? "black" : "gray"}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="[username]"
-        options={{
-          tabBarLabel: () => null,
-          tabBarIcon: ({ focused }) => (
-            <Ionicons
-              name="person-outline"
-              size={24}
-              color={focused ? "black" : "gray"}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="following"
-        options={{
-          tabBarLabel: () => null,
-          href: null,
-        }}
-      />
-      <Tabs.Screen
-        name="(post)/[username]/post/[postId]"
-        options={{
-          href: null,
-        }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="(home)"
+          options={{
+            tabBarLabel: () => null,
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                name="home"
+                size={24}
+                color={focused ? "black" : "gray"}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="search"
+          options={{
+            tabBarLabel: () => null,
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                name="search"
+                size={24}
+                color={focused ? "black" : "gray"}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="add"
+          listeners={{
+            tabPress: (e) => {
+              e.preventDefault();
+              if (isLoggedIn) {
+                router.navigate("./modal");
+              } else {
+                openLoginModal();
+              }
+            },
+          }}
+          options={{
+            tabBarLabel: () => null,
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                name="add"
+                size={24}
+                color={focused ? "black" : "gray"}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="activity"
+          listeners={{
+            tabPress: (e) => {
+              e.preventDefault();
+              if (!isLoggedIn) {
+                openLoginModal();
+              }
+            },
+          }}
+          options={{
+            tabBarLabel: () => null,
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                name="heart-outline"
+                size={24}
+                color={focused ? "black" : "gray"}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="[username]"
+          listeners={{
+            tabPress: (e) => {
+              e.preventDefault();
+              if (!isLoggedIn) {
+                openLoginModal();
+              }
+            },
+          }}
+          options={{
+            tabBarLabel: () => null,
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                name="person-outline"
+                size={24}
+                color={focused ? "black" : "gray"}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="following"
+          options={{
+            tabBarLabel: () => null,
+            href: null,
+          }}
+        />
+        <Tabs.Screen
+          name="(post)/[username]/post/[postId]"
+          options={{
+            href: null,
+          }}
+        />
+      </Tabs>
+      <Modal visible={isLoginModalOpen} transparent={true} animationType="fade">
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "flex-end",
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
+          }}
+        >
+          <View style={{ padding: 20, backgroundColor: "white" }}>
+            <Text>Login Modal</Text>
+            <TouchableOpacity onPress={closeLoginModal}>
+              <Ionicons name="close" size={24} color="#555" />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+    </>
   );
 }
